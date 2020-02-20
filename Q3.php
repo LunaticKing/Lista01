4c78edc7b9204981d5290ee3037a555bd6dc421a
Arthur Queiroz <arthur.queiroz@escolar.ifrn.edu.br>
2020-02-20 13:57:40 -0300

    Implementação da questão 3

---
+++ Q3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Questão 3</title>
+</head>
+<body>
+    <form action="recebedados.php" method="post">
+        <label for="usuario">Usuário:</label><br>
+        <input required type="text" name="usuario" id=""><br>
+        <label for="senha">Senha:</label><br>
+        <input required type="password" name="senha" id=""><br><br>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
---
+++ recebedados.php
@@ -0,0 +1,11 @@
+<?php
+    $usuario = $_POST["usuario"];
+    $senha = $_POST["senha"];
+
+    if (($usuario == "Maria"||$usuario == "maria")&&$senha==12345) {
+        echo "Autenticação realizada com sucesso";
+    } else {
+        echo "Você não tem permissão de visualizar essa página";
+    }
+    
+?>
